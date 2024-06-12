abb25e95afe6f342f3491b389212c42006cd253a
rfifxjr <53839280+rfifxjr@users.noreply.github.com>
2024-06-12 15:16:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,127 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+firefox_option = webdriver.FirefoxOptions()
+firefox_option.add_argument('--start-maximized')
+browser = webdriver.Firefox(options=firefox_option)
+
+browser.get('https://demo.opencart-ru.ru/index.php?route=common/home')
+
+time.sleep(1.5)
+
+element = browser.find_element(By.XPATH, "//div[@id='slideshow0']")
+element.click()
+
+time.sleep(1.5)
+
+element = browser.find_element(By.XPATH, "//li[2]//a[1]//img[1]")
+element.click()
+
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//button[@title='Next (Right arrow key)']")
+element.click()
+element.click()
+
+time.sleep(1.5)
+browser.back()
+
+time.sleep(1.5)
+
+actions = ActionChains(browser)
+PC = browser.find_element(By.XPATH,"//a[@class='dropdown-toggle'][contains(text(),'Компьютеры')]")
+actions.move_to_element(PC).perform()
+
+time.sleep(1.5)
+
+element = browser.find_element(By.XPATH, "//a[normalize-space()='PC (0)']")
+element.click()
+
+time.sleep(1)
+browser.back()
+
+time.sleep(1.5)
+
+element = browser.find_element(By.XPATH, "//a[@title='Личный кабинет']")
+element.click()
+
+time.sleep(1.5)
+
+element = browser.find_element(By.XPATH, "//ul[@class='dropdown-menu dropdown-menu-right']//a[contains(text(),'Регистрация')]")
+element.click()
+
+time.sleep(1.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_email']")
+element.send_keys("kosegarov@yandex.ru")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_password']")
+element.send_keys("123456789")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_confirm_password']")
+element.send_keys("123456789")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_firstname']")
+element.send_keys("Andrey")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_lastname']")
+element.send_keys("Kochegarov")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_telephone']")
+element.send_keys("89930021561")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//select[@id='register_country_id']")
+element.click()
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//option[@value='176']")
+element.click()
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//select[@id='register_zone_id']")
+element.click()
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//option[@value='83']")
+element.click()
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_city']")
+element.send_keys("Москва")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_postcode']")
+element.send_keys("14422")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@id='register_address_1']")
+element.send_keys("ВДНХ")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//a[@id='simpleregister_button_confirm']")
+element.click()
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@placeholder='Поиск']")
+element.click()
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//input[@placeholder='Поиск']")
+element.send_keys("htc")
+time.sleep(0.5)
+
+element = browser.find_element(By.XPATH, "//button[@class='btn btn-default btn-lg']")
+element.click()
+time.sleep(0.5)
+
+input("Нажмите Enter для завершения работы")
+
+browser.quit()
